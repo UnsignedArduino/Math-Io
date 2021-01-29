e1281989c93eca05d974348215d1ce7d648fa5bd
Ckyiu <UnsignedArduino@outlook.com>
2021-01-28 19:01:30 -0500

    Show cursor

---
+++ images.g.ts
@@ -0,0 +1,52 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "cursor":return img`
+2 . . . . . . . . . 
+f f . . . . . . . . 
+f 1 f . . . . . . . 
+f 1 1 f . . . . . . 
+f 1 1 1 f . . . . . 
+f 1 1 1 1 f . . . . 
+f 1 1 1 1 1 f . . . 
+f 1 1 1 1 1 1 f . . 
+f 1 1 1 1 1 1 1 f . 
+f 1 1 f 1 f f f f f 
+f 1 f f 1 f . . . . 
+f f . . f 1 f . . . 
+f . . . f 1 f . . . 
+. . . . . f 1 f . . 
+. . . . . f 1 f . . 
+. . . . . . f . . . 
+`;
+            case "image2":
+            case "cursor_pointer":return img`
+f 
+`;
+            case "image3":
+            case "cursor_box":return img`
+a a a a a a a a 
+a . . . . . . a 
+a . . . . . . a 
+a . . . . . . a 
+a . . . . . . a 
+a . . . . . . a 
+a . . . . . . a 
+a a a a a a a a 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,2 +1,33 @@
-tiles.setSmallTilemap(tilemap`map`)
-scene.setBackgroundColor(13)
+function make_cursor () {
+    sprite_cursor = sprites.create(assets.image`cursor`, SpriteKind.Player)
+    sprite_cursor_pointer = sprites.create(assets.image`cursor_pointer`, SpriteKind.Player)
+    sprite_cursor_box = sprites.create(assets.image`cursor_box`, SpriteKind.Player)
+    enable_cursor(true)
+    tiles.placeOnRandomTile(sprite_cursor_pointer, assets.tile`acceptor`)
+    scene.cameraFollowSprite(sprite_cursor_pointer)
+    sprite_cursor_pointer.setFlag(SpriteFlag.GhostThroughWalls, true)
+    sprite_cursor_pointer.setStayInScreen(true)
+}
+function enable_cursor (en: boolean) {
+    if (en) {
+        controller.moveSprite(sprite_cursor_pointer, 64, 64)
+    } else {
+        controller.moveSprite(sprite_cursor_pointer, 0, 0)
+    }
+}
+function make_map () {
+    tiles.setSmallTilemap(tilemap`map`)
+    scene.setBackgroundColor(13)
+}
+let sprite_cursor_box: Sprite = null
+let sprite_cursor_pointer: Sprite = null
+let sprite_cursor: Sprite = null
+make_map()
+make_cursor()
+game.onUpdate(function () {
+    sprite_cursor.top = sprite_cursor_pointer.top
+    sprite_cursor.left = sprite_cursor_pointer.left
+    sprite_cursor_box.y = sprite_cursor_pointer.top
+    sprite_cursor_box.x = sprite_cursor_pointer.left
+    tiles.placeOnTile(sprite_cursor_box, tiles.locationOfSprite(sprite_cursor_box))
+})

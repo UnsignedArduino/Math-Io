940ee8fbce43dc3c85b2e82fd3bd7ecf68d1b8e8
Ckyiu <UnsignedArduino@outlook.com>
2021-01-28 21:57:50 -0500

    Show score target and press A to move faster

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ function on_buildable_tile (sprite: Sprite) {
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     timer.background(function () {
         if (!(blockMenu.isMenuOpen())) {
-            enable_cursor(false)
+            enable_cursor(false, false)
             blockMenu.showMenu(["Cancel", "Delete", "Add"], MenuStyle.List, MenuLocation.BottomHalf)
             wait_for_select()
             if (blockMenu.selectedMenuIndex() == 1) {
@@ -29,9 +29,14 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
                     game.showLongText("Cannot place tile here!", DialogLayout.Bottom)
                 }
             }
-            enable_cursor(true)
+            enable_cursor(true, false)
         }
     })
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (controls_enabled) {
+        enable_cursor(true, true)
+    }
 })
 function on_deleteable_tile (sprite: Sprite) {
     for (let tile of [
@@ -54,16 +59,36 @@ function on_deleteable_tile (sprite: Sprite) {
     }
     return false
 }
+spriteutils.createRenderable(150, function (screen2) {
+    if (sprite_cursor_pointer.top > 12) {
+        screen2.fillRect(0, 0, 160, 12, 15)
+        screen2.drawLine(0, 12, 160, 12, 1)
+        images.print(screen2, "Score: " + info.score(), 2, 2, 1)
+    }
+    if (sprite_cursor_pointer.top < tiles.tilemapRows() * tiles.tileWidth() - 12 && !(blockMenu.isMenuOpen())) {
+        screen2.fillRect(0, scene.screenHeight() - 12, 160, scene.screenHeight(), 15)
+        screen2.drawLine(0, scene.screenHeight() - 12, 160, scene.screenHeight() - 12, 1)
+        images.print(screen2, "Target: " + target_number + " (Need " + target_needed + ")", 2, scene.screenHeight() - 10, 1)
+    }
+})
 function make_cursor () {
     sprite_cursor = sprites.create(assets.image`cursor`, SpriteKind.Player)
+    sprite_cursor.z = 99
     sprite_cursor_pointer = sprites.create(assets.image`cursor_pointer`, SpriteKind.Player)
+    sprite_cursor_pointer.z = 100
     sprite_cursor_box = sprites.create(assets.image`cursor_box`, SpriteKind.Player)
-    enable_cursor(true)
+    sprite_cursor_box.z = 98
+    enable_cursor(true, false)
     tiles.placeOnRandomTile(sprite_cursor_pointer, assets.tile`acceptor`)
     scene.cameraFollowSprite(sprite_cursor_pointer)
     sprite_cursor_pointer.setFlag(SpriteFlag.GhostThroughWalls, true)
     sprite_cursor_pointer.setStayInScreen(true)
 }
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    if (controls_enabled) {
+        enable_cursor(true, false)
+    }
+})
 function ask_for_conveyor () {
     while (true) {
         blockMenu.showMenu([
@@ -147,9 +172,14 @@ function ask_for_conveyor () {
         }
     }
 }
-function enable_cursor (en: boolean) {
+function enable_cursor (en: boolean, speed: boolean) {
+    controls_enabled = en
     if (en) {
-        controller.moveSprite(sprite_cursor_pointer, 64, 64)
+        if (speed) {
+            controller.moveSprite(sprite_cursor_pointer, 96, 96)
+        } else {
+            controller.moveSprite(sprite_cursor_pointer, 64, 64)
+        }
     } else {
         controller.moveSprite(sprite_cursor_pointer, 0, 0)
     }
@@ -169,14 +199,20 @@ blockMenu.onMenuOptionSelected(function (option, index) {
     selected = true
 })
 let selected = false
-let sprite_cursor_pointer: Sprite = null
 let sprite_cursor: Sprite = null
+let sprite_cursor_pointer: Sprite = null
+let controls_enabled = false
 let sprite_cursor_box: Sprite = null
+let target_needed = 0
+let target_number = 0
 let last_placed_belt: Image = null
 make_map()
 make_cursor()
 blockMenu.setColors(1, 15)
 last_placed_belt = assets.tile`conveyor_up`
+info.setScore(0)
+target_number = 2
+target_needed = 30
 game.onUpdate(function () {
     sprite_cursor.top = sprite_cursor_pointer.top
     sprite_cursor.left = sprite_cursor_pointer.left
